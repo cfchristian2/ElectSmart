43ba91ea98bd28f716b80de6c0616624e84215c2
Stephen G <stephengaschignard@gmail.com>
2016-03-13 17:11:26 -0500

    Added two more views and controllers \

---
+++ iOS/SmartElect/SmartElect/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  SmartElect
+//
+//  Created by Stephen Gaschignard on 3/13/16.
+//  Copyright © 2016 Stephen Gaschignard. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
