ba825113c0d4710a2bd8a579270f9dedf8ef04de
Josh Kasuboski <kasuboski@wisc.edu>
2016-03-13 10:59:04 -0500

    Have getting an address and moving on working in the happy case

--- Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/MainActivity.java
+++ Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/MainActivity.java
@@ -6,17 +6,12 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Handler;
 import android.os.ResultReceiver;
-import android.support.design.widget.TabLayout;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.electsmart.electsmart.geolocation.FetchAddressIntentService;
@@ -25,22 +20,9 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+public class MainActivity extends AppCompatActivity  implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private GoogleApiClient mGoogleApiClient;
 
@@ -48,23 +30,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private AddressResultReceiver mResultReceiver;
     String mAddressOutput;
 
+    private TextView mAddressText;
+    private Button mContinueButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        mAddressText = (TextView) findViewById(R.id.addressInfo);
+        mContinueButton = (Button) findViewById(R.id.continueButton);
+        mContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToNextView();
+            }
+        });
 
         mResultReceiver = new AddressResultReceiver(new Handler());
 
@@ -103,8 +84,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
 
                 //if (mAddressRequested) {
-                    startIntentService();
-               // }
+                startIntentService();
+                // }
             }
 
             Toast.makeText(MainActivity.this, String.valueOf(mLastLocation.getLatitude()) + ", " + String.valueOf(mLastLocation.getLongitude()), Toast.LENGTH_SHORT).show();
@@ -118,6 +99,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
     protected void startIntentService() {
         Intent intent = new Intent(this, FetchAddressIntentService.class);
         intent.putExtra(GeolocationConstants.RECEIVER, mResultReceiver);
@@ -125,35 +111,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         startService(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void moveToNextView() {
+        Intent intent = new Intent(this, TabActivity.class);
+        startActivity(intent);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        finish();
     }
 
     private void showToast(String message) {
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void displayAddressOutput() {
+        mAddressText.setText(mAddressOutput);
     }
 
     @SuppressLint("ParcelCreator")
@@ -174,54 +144,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             // Show a toast message if an address was found.
             if (resultCode == GeolocationConstants.SUCCESS_RESULT) {
                 showToast(getString(R.string.address_found));
+                displayAddressOutput();
+            } else if (resultCode == GeolocationConstants.FAILURE_RESULT) {
+                //TODO handle the case with no address
+                DialogFragment newFragment = new AddressDialogFragment();
+                newFragment.show(getSupportFragmentManager(), "address");
             }
 
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return HomeFragment.newInstance();
-                case 1:
-                    return UpcomingElectionsFragment.newInstance();
-                case 2:
-                    return CurrentEventsFragment.newInstance();
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Home";
-                case 1:
-                    return "Upcoming Elections";
-                case 2:
-                    return "Current Events";
-            }
-            return null;
         }
     }
 }
