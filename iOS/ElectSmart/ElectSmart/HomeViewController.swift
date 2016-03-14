5a70109d5cf738f03104b0e9bfa21fecfc10f1aa
Stephen G <stephengaschignard@gmail.com>
2016-03-13 22:18:44 -0500

    Finishing refactoring (making sure all files are included)

---
+++ iOS/ElectSmart/ElectSmart/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  ElectSmart
+//
+//  Created by Stephen Gaschignard on 3/8/16.
+//  Copyright © 2016 Stephen Gaschignard. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
