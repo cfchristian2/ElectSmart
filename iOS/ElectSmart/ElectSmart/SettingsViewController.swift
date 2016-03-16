2ea6a9e508f9ba87a9b978e3544f94d20a8441aa
Bjnkrtnoit Bbbbbbb <Christianson@conners-macbook-pro.local>
2016-03-16 13:08:50 -0500

    Added functionality to settings page, still need to correctly imbed in a scroll view

--- iOS/ElectSmart/ElectSmart/SettingsViewController.swift
+++ iOS/ElectSmart/ElectSmart/SettingsViewController.swift
@@ -12,6 +12,14 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        title = "Next Date"
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 139.0/255.0, blue: 139.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -20,5 +28,14 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: - Configure scope
+    
+    @IBOutlet var localScope: UIView!
+    
+    @IBOutlet var stateScope: UIView!
+    
+    @IBOutlet var nationalScope: UIView!
+    
+    //TODO: Filter elections
     
 }
