29e32a6f0a47e29a80de98f53ec22aa7472511af
Brandon Schaefer <bschaef14@gmail.com>
2016-03-17 09:47:55 -0500

    fixed issue with current events listView not being filled

--- Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/Adapters/CurrentEventsAdapter.java
+++ Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/Adapters/CurrentEventsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,23 +17,42 @@ import java.util.ArrayList;
 /**
  * Created by Scott on 3/11/2016.
  */
-public class CurrentEventsAdapter extends ArrayAdapter<CurrentEvent> {
+public class CurrentEventsAdapter extends BaseAdapter{
     private Context context;
+    private LayoutInflater mInflater;
     private ArrayList<CurrentEvent> currentEvents;
 
     public CurrentEventsAdapter(Context context, ArrayList<CurrentEvent> currentEvents) {
-        super(context, 0, currentEvents);
+        System.out.println("CurrentEvents: IN CONSTRUCTOR");
         this.context = context;
+        this.mInflater = LayoutInflater.from(context);
         this.currentEvents = currentEvents;
+    }
+
+    @Override
+    public int getCount(){
+        System.out.println("CurrentEvents: IN GET COUNT " + currentEvents.size());
+        return currentEvents.size();
+    }
+
+    @Override
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position){
+        return currentEvents.get(position);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        CurrentEvent currentEvent = getItem(position);
+        CurrentEvent currentEvent = currentEvents.get(position);
+        System.out.println("CurrentEvents: IN GET VIEW");
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.current_events_list_item, parent, false);
+            convertView = mInflater.inflate(R.layout.current_events_list_item, parent, false);
         }
         // Lookup view for data population
         /*
