91ca03e2ee4709478adc1d786cfe1c7b5da71c8d
Stephen G <stephengaschignard@gmail.com>
2016-03-17 12:01:24 -0500

    Modified the UpcomingViewController so that the cells in the collection appear properly

--- iOS/ElectSmart/ElectSmart/UpcomingViewController.swift
+++ iOS/ElectSmart/ElectSmart/UpcomingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UpcomingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class UpcomingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     //Collection view containing cells of elections
     @IBOutlet weak var collectionView: UICollectionView!
@@ -24,6 +24,8 @@ class UpcomingViewController: UIViewController, UICollectionViewDelegate, UIColl
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 139.0/255.0, blue: 139.0/255.0, alpha: 1.0)
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        
+        collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +39,7 @@ class UpcomingViewController: UIViewController, UICollectionViewDelegate, UIColl
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! UpcomingElectionCollectionViewCell
+        cell.backgroundColor = UIColor.redColor()
         
         cell.title?.text = self.titles[indexPath.row]
         cell.date?.text = self.dates[indexPath.row]
