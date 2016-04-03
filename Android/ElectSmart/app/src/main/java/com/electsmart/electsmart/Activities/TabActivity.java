b2e47b9bdce346f25570ab6d870331cba2cb9347
Josh Kasuboski <kasuboski@wisc.edu>
2016-04-03 14:16:12 -0500

    Added back polling place and added code coverage

--- Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/Activities/TabActivity.java
+++ Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/Activities/TabActivity.java
@@ -102,20 +102,20 @@ public class TabActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        if(id == R.id.action_polling_place){
-//                Intent intent = new Intent(this, PollingPlaceActivity.class);
-//                this.startActivity(intent);
-//                return true;
-//        }
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.action_polling_place){
+                Intent intent = new Intent(this, PollingPlaceActivity.class);
+                this.startActivity(intent);
+                return true;
+        }
 //
 //        return super.onOptionsItemSelected(item);
 
