a7f8a92efba68c60fbdcfb6ff5ffd3088232cb38
Stephen G <stephengaschignard@gmail.com>
2016-04-05 16:24:28 -0500

    Changed home view news story cells to display article content

--- iOS/ElectSmart/ElectSmart/HomeStoryTableViewController.swift
+++ iOS/ElectSmart/ElectSmart/HomeStoryTableViewController.swift
@@ -36,62 +36,15 @@ class HomeStoryTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("homeStoryCell", forIndexPath: indexPath)
-        
-        // TODO: Change configuration here so that the proper image/title/URL are set for cell
-        // TODO: Have UITableViewCell resize to height of downloaded image (currently set to fixed height)
+        let cell = tableView.dequeueReusableCellWithIdentifier("homeStoryCell", forIndexPath: indexPath) as! HomeStoryTableViewCellController
         
         // get story for cell
         let story: NewsStory = self.stories[indexPath.row]
         
-        // Configure the cell
-        let imageView = UIImageView(frame: cell.frame)
-        let labelView = UILabel()
-        
-        // Add image BG
-        if let checkedUrl = NSURL(string: story.image!) {
-            imageView.contentMode = .ScaleAspectFit
-            downloadImage(checkedUrl, imageView: imageView)
-        }
-        
-        // Add text overlay
-        drawTitle(story.title, labelView: labelView, imageViewToReference: imageView)
-        
-        // Layer subviews
-        self.view.addSubview(imageView)
-        self.view.addSubview(labelView)
+        cell.newsStoryTitleLabel.text = "  "+story.title
+        cell.newsStoryContentLabel.text = story.content
         
         return cell
-    }
-    
-    // MARK: Drawing text overlay
-    
-    private func drawTitle(title: String, labelView: UILabel, imageViewToReference: UIImageView) {
-        labelView.text = "    "+title
-        labelView.font = UIFont(name: "System", size: 17)
-        labelView.textColor = UIColor.blackColor()
-        labelView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.6)
-        labelView.textAlignment = .Left
-        labelView.numberOfLines = 1
-        labelView.frame = CGRectMake(imageViewToReference.frame.minX, (imageViewToReference.frame.minY+imageViewToReference.frame.height-40), imageViewToReference.frame.width, 40)
-    }
-    
-    // MARK: Loading images into TableView
-    
-    private func getDataFromUrl(url: NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError?) -> Void)) {
-        NSURLSession.sharedSession().dataTaskWithURL(url) {
-            (data, response, error) in completion(data: data, response: response, error: error)
-            }.resume()
-    }
-    
-    private func downloadImage(url: NSURL, imageView: UIImageView) {
-        getDataFromUrl(url) { (data, response, error)  in
-            dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                guard let data = data where error == nil else { return }
-                imageView.image = UIImage(data: data)
-                self.tableView.layoutSubviews()
-            }
-        }
     }
     
     // MARK: - Navigation
