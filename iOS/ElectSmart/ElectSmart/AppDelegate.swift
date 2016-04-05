b840fa76ca22d6aa08ac39dd3a5d5ab1da1bea6c
Nicholas <nasieb@wisc.edu>
2016-04-04 20:59:07 -0500

    Alamofire Fix

--- iOS/ElectSmart/ElectSmart/AppDelegate.swift
+++ iOS/ElectSmart/ElectSmart/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 import Fabric
 import Crashlytics
 import TwitterKit
+import Alamofire
+
 
 
 @UIApplicationMain
--- iOS/ElectSmart/Pods/Target Support Files/Pods-ElectSmart/Pods-ElectSmart-frameworks.sh
+++ iOS/ElectSmart/Pods/Target Support Files/Pods-ElectSmart/Pods-ElectSmart-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-ElectSmart/AFNetworking.framework"
+  install_framework "Pods-ElectSmart/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-ElectSmart/AFNetworking.framework"
+  install_framework "Pods-ElectSmart/Alamofire.framework"
 fi
