79c5c8085984863f4bde9df855b2dd281fd0bfae
Scott Franklin <runfranksrun525@live.com>
2016-04-06 14:56:51 -0500

    refactored some bingTask code

--- Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/API/bing/BingTask.java
+++ Android/ElectSmart/app/src/main/java/com/electsmart/electsmart/API/bing/BingTask.java
@@ -60,33 +60,24 @@ public class BingTask extends AsyncTask<Void, Void, String> {
                 Gson gson = (new GsonBuilder()).create();
                 BingSearchResults bingResult = gson.fromJson(result, BingSearchResults.class);
                 if(isHome){
-                    String eventTitle = bingResult.d.results[0].Title;
-                    String eventDescription = bingResult.d.results[0].Description;
-                    TextView titleText = (TextView) context.findViewById(R.id.EventTitleText);
-                    titleText.setText(eventTitle);
-                    TextView description = (TextView) context.findViewById(R.id.MainEventDescription);
-                    description.setText(eventDescription);
+                    BingSearchResults.Result mainEventResult = bingResult.d.results[0];
+                    String eventTitle = mainEventResult.Title;
+                    String eventDescription = mainEventResult.Description;
+                    HandleSetupForHomeMainEvent(eventTitle, eventDescription);
                 }else{
-                    ArrayList<BingSearchResults.Result> resultsConverted = new ArrayList<>();
+                    List<BingSearchResults.Result> resultsConverted = new ArrayList<>();
                     BingSearchResults.Result[] results = bingResult.d.results;
                     for(int i = 1; i < results.length; i++){
                         resultsConverted.add(results[i]);
                     }
-                    currentEventsAdapter.clear();
-                    currentEventsAdapter.addAll(resultsConverted);
-                    currentEventsAdapter.notifyDataSetChanged();
+                    HandleCurrentEventsAdapterSetup(resultsConverted);
                 }
             }else{
                 if(isHome){
-                    TextView titleText = (TextView) context.findViewById(R.id.EventTitleText);
-                    titleText.setText(" ");
-                    TextView description = (TextView) context.findViewById(R.id.MainEventDescription);
-                    description.setText(" ");
+                    HandleSetupForHomeMainEvent(" ", " ");
                 }else{
                     List<BingSearchResults.Result> results = new ArrayList<>();
-                    currentEventsAdapter.clear();
-                    currentEventsAdapter.addAll(results);
-                    currentEventsAdapter.notifyDataSetChanged();
+                    HandleCurrentEventsAdapterSetup(results);
                 }
 
             }
@@ -96,6 +87,19 @@ public class BingTask extends AsyncTask<Void, Void, String> {
 
             //throw new NullPointerException();
         }
+    }
+
+    private void HandleSetupForHomeMainEvent(String eventTitle, String eventDescription) {
+        TextView titleText = (TextView) context.findViewById(R.id.EventTitleText);
+        titleText.setText(eventTitle);
+        TextView descriptionText = (TextView) context.findViewById(R.id.MainEventDescription);
+        descriptionText.setText(eventDescription);
+    }
+
+    private void HandleCurrentEventsAdapterSetup(List<BingSearchResults.Result> resultsConverted) {
+        currentEventsAdapter.clear();
+        currentEventsAdapter.addAll(resultsConverted);
+        currentEventsAdapter.notifyDataSetChanged();
     }
 
     private void getImage(String ImageUrl){
