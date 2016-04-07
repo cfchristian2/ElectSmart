977e3080fbd4c102e88c3ba06f7251fcb1b904a9
root <stfranklin@wisc.edu>
2016-04-07 03:39:45 +0000

    added HomeNavigationTest.java, not working

---
+++ Android/ElectSmart/app/src/androidTest/java/com/electsmart/electsmart/HomeNavigationTest.java
@@ -0,0 +1,81 @@
+package com.electsmart.electsmart;
+
+import android.app.Application;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.test.ApplicationTestCase;
+import android.util.Log;
+
+import com.electsmart.electsmart.Activities.TabActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Scott  on 4/6/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+
+public class HomeNavigationTest extends ApplicationTestCase<Application> {
+    public HomeNavigationTest() {
+        super(Application.class);
+    }
+    private UiDevice mDevice;
+
+    @Rule
+    public IntentsTestRule<TabActivity> mActivityRule = new IntentsTestRule<TabActivity>(
+            TabActivity.class){
+        @Override
+        protected void beforeActivityLaunched() {
+            mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+            super.beforeActivityLaunched();
+        }
+    };
+
+    @Test
+    public void OpenCurrentEventsArticleTest() {
+        UiObject homeTab = mDevice.findObject(new UiSelector().className("android.support.v7.app.ActionBar$Tab").index(0));
+        try {
+            if (homeTab.exists() && homeTab.isEnabled()) {
+                homeTab.click();
+            }
+        } catch (UiObjectNotFoundException e) {
+            System.exit(1);
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            //do nothing
+        }
+
+        UiObject mainArticle = mDevice.findObject(new UiSelector()
+                .resourceId("MainEventPeek"));
+        try {
+            if (mainArticle.exists() && mainArticle.isEnabled()) {
+                mainArticle.click();
+            }
+            else{
+                System.exit(1);
+            }
+        }catch (UiObjectNotFoundException e){
+            Log.d("SETTINGS ERROR: ", "Couldn't find settings option");
+        }
+        catch (Exception ex){
+            Log.d("error", "some error");
+        }
+
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e) {
+            //do nothing
+        }
+    }
+}
--- Android/ElectSmart/app/src/androidTest/java/com/electsmart/electsmart/test/CurrentEventsNavTest.java
+++ Android/ElectSmart/app/src/androidTest/java/com/electsmart/electsmart/test/CurrentEventsNavTest.java
@@ -1,46 +1,20 @@
 package com.electsmart.electsmart.test;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import android.content.Context;
-import android.content.Intent;
-import android.support.design.widget.TabLayout;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.espresso.matcher.BoundedMatcher;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.SdkSuppress;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
-import android.support.test.uiautomator.Until;
 import android.util.Log;
 
-import com.electsmart.electsmart.*;
-import com.electsmart.electsmart.API.GoogleCivicInfo.Models.Candidate;
-import com.electsmart.electsmart.Activities.MainActivity;
 import com.electsmart.electsmart.Activities.TabActivity;
-import com.electsmart.electsmart.Models.Election;
-import com.electsmart.electsmart.Models.UpcomingElectionRow;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
@@ -61,7 +35,7 @@ public class CurrentEventsNavTest {
     @Test
     public void OpenCurrentEventsArticleTest() {
         //Open More options menu
-        UiObject currentEventsTab= mDevice.findObject(new UiSelector().className("android.support.v7.app.ActionBar$Tab").index(2));
+        UiObject currentEventsTab = mDevice.findObject(new UiSelector().className("android.support.v7.app.ActionBar$Tab").index(2));
         try {
             if (currentEventsTab.exists() && currentEventsTab.isEnabled()) {
                 currentEventsTab.click();
